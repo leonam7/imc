d00fb4255cc837379c17e66ac3c203cb75b8d865
Leonam P <80845786+leonam7@users.noreply.github.com>
2022-06-14 19:20:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 <body>
     <nav class="navbar navbar-light" style="background-color:#00BFFF;">
         <a class="navbar-brand" href="#">
-          <img  src="img/Science-Scale-icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
-          Calcule aqui seu IMC
+          Calcule seu IMC
         </a>
     </nav>
 <div class="container">
