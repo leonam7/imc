8f65f2274d256062b2bdad61184eb781ed111f7f
Leonam P <80845786+leonam7@users.noreply.github.com>
2022-06-14 23:14:16 -0300

    Update imc.php

--- imc.php
+++ imc.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    
     <nav class="navbar navbar-light" style="background-color:#00BFFF;">
         <a class="navbar-brand" href="#">
           Calcule aqui seu IMC
